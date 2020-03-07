44dcd373592b0d7a7ea2c533d0783fc00ff54885
WL <walldi@tlen.pl>
2020-03-07 11:11:49 +0100

    dystans

--- src/main/java/pl/sda/student/Point.java
+++ src/main/java/pl/sda/student/Point.java
@@ -1,4 +1,31 @@
 package pl.sda.student;
 
 public class Point {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point p1) {
+        //  System.out.println(this.x);
+        // System.out.println(this.y);
+        double x = p1.getX() - this.x;
+        double y = p1.getY() - this.x;
+
+        Math.sqrt((x * x) + (y * y));
+
+        // System.out.println(p1.getX());
+        // System.out.println(p1.getY());
+    }
+
 }
